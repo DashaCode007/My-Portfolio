ad9750bd603e0407273375fed13eb779638f6608
DashaCode007 <88.jrlorica881@gmail.com>
2023-08-14 22:29:16 +0800

    08/14/23 new codes

--- script/main.js
+++ script/main.js
@@ -17,3 +17,12 @@ document.addEventListener("DOMContentLoaded", function(){
     AOS.refresh();
 });
 
+document.addEventListener("DOMContentLoaded", function(){
+  var myOffcanvas = document.getElementById('offcanvasExample');
+  var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas);
+  document.getElementById("OpenMenu").addEventListener('click',function (e){
+    e.preventDefault();
+    e.stopPropagation();
+    bsOffcanvas.toggle();
+  });
+});
